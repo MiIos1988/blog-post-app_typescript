fc06bf41c4d7d9f04f7cfce1d487f3c16820825a
Milos1988 <vojvoda1988@gmail.com>
2023-12-17 23:54:54 +0100

    Corrected code.

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,27 @@ import PostLayout from "./components/PostLayout";
 import Sidebar from "./components/Sidebar";
 
 function App() {
-
   const [sidebarData, setSidebarData] = useState();
   const [postData, setPostData] = useState([]);
   const [currentTag, setCurrentTag] = useState("");
 
   const selectTag = (tag: string) => {
-    setCurrentTag(tag)
-  }
+    setCurrentTag(tag);
+  };
 
   useEffect(() => {
     fetch("https://raw.githubusercontent.com/zile028/fake-db/main/tags")
-      .then(res => res.json())
-      .then(data => {setSidebarData(data)})
-  }, []
-  );
+      .then((res) => res.json())
+      .then((data) => {
+        setSidebarData(data);
+      });
+  }, []);
 
   useEffect(() => {
     fetch("https://raw.githubusercontent.com/zile028/fake-db/main/posts")
-      .then(res => res.json())
-      .then(data => setPostData(data))
-  }, []
-  )
+      .then((res) => res.json())
+      .then((data) => setPostData(data));
+  }, []);
 
   return (
     <>
@@ -34,13 +33,22 @@ function App() {
       <div className="container-fluid ">
         <div className="row">
           <div className="col-2">
-            {sidebarData && <Sidebar sidebarData={sidebarData} selectTag={selectTag} currentTag={currentTag}/>}
+            {sidebarData && (
+              <Sidebar
+                sidebarData={sidebarData}
+                selectTag={selectTag}
+                currentTag={currentTag}
+              />
+            )}
           </div>
-          <PostLayout postData={postData} currentTag={currentTag} selectTag={selectTag} />
+          <PostLayout
+            postData={postData}
+            currentTag={currentTag}
+            selectTag={selectTag}
+          />
         </div>
       </div>
     </>
-
   );
 }
 
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,28 +1,42 @@
-import React from 'react'
+import React from "react";
 
 interface Post {
-    id: number;
-    title: string;
-    body: string;
-    userId: number;
-    tags: string[];
-    reactions: number;
-  }
-
-export const Post = ({ post, selectTag }: {post: Post, selectTag: (tag: string) => void;}) => {
-    return (
-
-        <div className='col-3 m-2' >
-            <div className="card ">
-                <div className="card-header"><p>{post.title}</p></div>
-                <div className="card-body">{post.body}</div>
-                <div className="card-footer">
-                    {post.tags.map((tag, index) => {
-                        return <button key={index} className='btn btn-sm btn-warning m-2' onClick={() => selectTag(tag)}>{tag}</button>
-                    }
-                    )}
-                </div>
-            </div>
-        </div>
-    )
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+  tags: string[];
+  reactions: number;
 }
+
+export const Post = ({
+  post,
+  selectTag,
+}: {
+  post: Post;
+  selectTag: (tag: string) => void;
+}) => {
+  return (
+    <div className="col-3 m-2">
+      <div className="card ">
+        <div className="card-header">
+          <p>{post.title}</p>
+        </div>
+        <div className="card-body">{post.body}</div>
+        <div className="card-footer">
+          {post.tags.map((tag, index) => {
+            return (
+              <button
+                key={index}
+                className="btn btn-sm btn-warning m-2"
+                onClick={() => selectTag(tag)}
+              >
+                {tag}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
